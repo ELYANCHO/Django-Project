01323c31ed68cc027f1f9610e53d9f14b7cde2c3
Elyan Alejandro Cristancho Castañeda <elyancristancho123@gmail.com>
2025-11-14 10:28:19 -0500

    Tercer commit, puede que sea el ultimo

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -5,5 +5,6 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
-
-
+  favorite_game = models.CharField(null=True)
+  playing_hours = models.IntegerField(null=True)
+  game_gender = models.CharField(null=True)
